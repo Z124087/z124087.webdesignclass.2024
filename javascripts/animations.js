e08d6dea622773e32f4990edbf511d9cd496dfdf
z124087Rudolf <167381774+Z124087@users.noreply.github.com>
2024-05-25 17:28:38 +0900

    Delete javascripts directory

--- javascripts/animations.js
+++
@@ -1,9 +0,0 @@
-
-function toggleText(elementId) {
-    var hiddenText = document.getElementById(elementId);
-    if (hiddenText.style.display === "none") {
-        hiddenText.style.display = "block";
-    } else {
-        hiddenText.style.display = "none";
-    }
-}
